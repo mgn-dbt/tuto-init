81afd5dde717b261ed571a5a1930f72e4357e4d5
GUEDON Marc <marc.guedon@cgi.com>
2026-06-17 17:04:59 +0200

    correction macro

--- macros/grant_select.sql
+++ macros/grant_select.sql
@@ -4,17 +4,18 @@
 
     {% if execute %}
         {% if target.type == 'postgres' %}
-            {% set sql %}
-                GRANT USAGE ON SCHEMA {{ schema }} TO {{ role }};
-                GRANT SELECT ON all tables IN schema {{ schema }} TO {{ role }};
-            {% endset %}
 
             {% for agate_row in list_schemas(target.database) %}
 
                 {% set schema = agate_row.values()[0] %}
+
+                {% set sql %}
+GRANT USAGE ON SCHEMA {{ schema }} TO {{ role }};
+GRANT SELECT ON all tables IN schema {{ schema }} TO {{ role }};
+                {% endset %}
                 {# 
-                if schema.startswith(target.schema)             (python)
-                if schema.replace(target.schema, '') != schema  (jinja)
+                    startswith alternative
+                    schema.replace(target.schema, '') != schema
                 #}
                 {% if schema.startswith(target.schema) %}
                     {{ log("Granting Select on schema " ~ schema ~ " to role " ~ role, info=True) }}
